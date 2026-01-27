726fbe33e6d5e6e1b9a73364e1109aef5a14e951
OrhanKaynak <kaynakorhan354@gmail.com>
2026-01-27 22:34:30 +0300

    27-01-26 Encapsulation

---
+++ Konu13KapsullemeEncapsulation/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Konu13KapsullemeEncapsulation
+{
+    internal class Bolum
+    {
+        private string BolumAdi; // dışarıdan erişime kapalı değişkenimiz.
+
+        public string GetBolumAdi()
+        { 
+            return BolumAdi; //GetBolumAdi çağırılınca BolumAdi değişkenini yolla 
+        }
+
+        public void SetBolumAdi(string istenenEgitim)
+        {
+            if (istenenEgitim == "Yazılım Eğitimi") // Mutator (setter) seçilen eğitime izin verildi. 
+            {
+                BolumAdi = istenenEgitim;
+            }
+            else
+            {
+                Console.WriteLine("Kurumumuzda bu eğitim verilmemektedir");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Konu13 Kapsulleme Encapsulation");
+            Console.WriteLine("Metot ile Kapsulleme");
+            Bolum bolum = new Bolum();
+            Console.WriteLine("Hangi alanda eğitim almak istersiniz?");
+            var bolumAdi = Console.ReadLine();
+            bolum.SetBolumAdi(bolumAdi);
+            Console.WriteLine("Bölüm: " + bolum.GetBolumAdi());
+        }
+    }
+}
