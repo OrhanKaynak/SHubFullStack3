62aebfa6b14777bc1276e33427b9835532f48ff0
OrhanKaynak <kaynakorhan354@gmail.com>
2026-01-28 22:30:22 +0300

    28-01-26 Interfaces

--- Konu13KapsullemeEncapsulation/Program.cs
+++ Konu13KapsullemeEncapsulation/Program.cs
@@ -27,11 +27,38 @@
         {
             Console.WriteLine("Konu13 Kapsulleme Encapsulation");
             Console.WriteLine("Metot ile Kapsulleme");
-            Bolum bolum = new Bolum();
+            Bolum bolum = new Bolum(); //Bolum classından bolum adında bir nesne üret
             Console.WriteLine("Hangi alanda eğitim almak istersiniz?");
-            var bolumAdi = Console.ReadLine();
-            bolum.SetBolumAdi(bolumAdi);
-            Console.WriteLine("Bölüm: " + bolum.GetBolumAdi());
+            var bolumAdi = Console.ReadLine(); // ekrandan girilecek değeri oku
+            bolum.SetBolumAdi(bolumAdi); // girilen değeri bolum nesnesindeki set metoduna gonder
+            Console.WriteLine("Bölüm: " + bolum.GetBolumAdi()); // bolum nesnesindeki metotla private değişkenin değerini oku
+
+            Console.WriteLine();
+
+            // property ile kapsülleme
+            Console.WriteLine("property ile kapsülleme");
+            Fakulte fakulteNesnesi = new Fakulte();
+            fakulteNesnesi.Bolum = bolumAdi; // veri atama: set bloğunu çalıştırır.
+            Console.WriteLine("Bölüm: " + fakulteNesnesi.Bolum); // veri okuma: get bloğunu çalıştırır
+        }
+    }
+    public class Fakulte
+    {
+        private string bolum;
+        public string Bolum
+        { 
+            get { return bolum;}
+            set 
+            {
+                if(value == "Yazılım Eğitimi")  // eğer gönderilen değer Yazılı Eğitimi ise
+                {
+                    bolum = value; // property e değer atamasına izin ver
+                }
+                else 
+                {
+                    Console.WriteLine("Kurumumuzda bu eğitim verilmemektedir!");
+                }
+            }
         }
     }
 }
---
+++ Konu14InterfacesArayuzler/ISinifGereksinimleri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konu14InterfacesArayuzler
+{
+    internal interface ISinifGereksinimleri // bu interface her class ta bulunması gereken zorunlu özellikleri içeriyor
+    {
+        public int Id { get; set; } // nesnenin benzersiz kimlik değeri
+        public DateTime CreateDate { get; set; } // nesnenin oluşma zamanını tutacak özellik
+        public DateTime UpdateDate { get; set; } // nesnenin son güncelleme zamanını tutacak özellik
+        public bool IsActive { get; set; } // nesnenin aktiflik durumunu tutacak olan özellik
+    }
+}
