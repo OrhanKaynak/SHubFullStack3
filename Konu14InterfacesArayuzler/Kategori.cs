d5ce1b007f074d14b7650095e9c61e8efa7356a0
OrhanKaynak <kaynakorhan354@gmail.com>
2026-01-29 22:36:28 +0300

    29-01-26 Collections

---
+++ Konu14InterfacesArayuzler/Kategori.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konu14InterfacesArayuzler
+{
+    internal class Kategori : ISinifGereksinimleri
+    {
+        public int Id { get; set; } // nesnenin benzersiz kimlik değeri
+        public DateTime CreateDate { get; set; } // nesnenin oluşma zamanını tutacak özellik
+        public DateTime UpdateDate { get; set; } // nesnenin son güncelleme zamanını tutacak özellik
+        public bool IsActive { get; set; } // nesnenin aktiflik durumunu tutacak olan özellik
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+    }
+}
---
+++ Konu14InterfacesArayuzler/Urun.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konu14InterfacesArayuzler
+{
+    internal class Urun : ISinifGereksinimleri
+    {
+        public int Id { get; set; } // nesnenin benzersiz kimlik değeri
+        public DateTime CreateDate { get; set; } // nesnenin oluşma zamanını tutacak özellik
+        public DateTime UpdateDate { get; set; } // nesnenin son güncelleme zamanını tutacak özellik
+        public bool IsActive { get; set; } // nesnenin aktiflik durumunu tutacak olan özellik
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
