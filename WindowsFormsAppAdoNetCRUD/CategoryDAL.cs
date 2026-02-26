57f9aee214eee225e8c11b2dacdfb59248a912cc
OrhanKaynak <kaynakorhan354@gmail.com>
2026-02-26 22:31:00 +0300

    26-02-26 ado.net crud 1

---
+++ WindowsFormsAppAdoNetCRUD/CategoryDAL.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace WindowsFormsAppAdoNetCRUD
+{
+    internal class CategoryDAL : OrtakDAL
+    {
+        public int Add(Category category)
+        {
+            int sonuc = 0;
+
+            ConnectionKontrol();
+            var command = new SqlCommand("insert into categories (Name, Description, IsActive, CreateDate) values (@Name, @Description, @IsActive, @CreateDate)", _connection);
+            command.Parameters.AddWithValue("@Name", category.Name);
+            command.Parameters.AddWithValue("@Description", category.Description);
+            command.Parameters.AddWithValue("@IsActive", category.IsActive);
+            command.Parameters.AddWithValue("@CreateDate", category.CreateDate);
+            sonuc = command.ExecuteNonQuery();
+            command.Dispose();
+            _connection.Close();
+
+            return sonuc;
+        }
+    }
+}
