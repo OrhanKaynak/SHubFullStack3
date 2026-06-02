4aa62d7efdbfbce51d8aa459fb1d085d63b21a2b
OrhanKaynak <kaynakorhan354@gmail.com>
2026-06-02 22:03:14 +0300

    02-06-26 cshtml

---
+++ NetCoreMVCEgitimi/Controllers/MVCRazorSyntaxController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreMVCEgitimi.Controllers
+{
+    public class MVCRazorSyntaxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
