35add5bdd599dd9f093a75dff8172fdc78dd0259
OrhanKaynak <kaynakorhan354@gmail.com>
2026-06-13 18:39:55 +0300

    13-06-26  results

---
+++ NetCoreMVCEgitimi/Controllers/MVC09ViewResultsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreMVCEgitimi.Models;
+
+namespace NetCoreMVCEgitimi.Controllers
+{
+    public class MVC09ViewResultsController : Controller
+    {
+        UyeContext db = new UyeContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult FarkliViewDondur()
+        {
+            return View("Index");
+        }
+        public IActionResult Yonlendir()
+        {
+            // Bir action içerisinde farklı bir sayfaya yönlendirme yapabiliriz
+            // return Redirect("/Home");
+            return Redirect("https://www.google.com.tr/index.html");
+        }
+        public IActionResult ActionaYonlendir()
+        {
+            // Bir action içerisinde farklı bir Actiona yönlendirme yapabiliriz
+            // return Redirect("/Home");
+            // return RedirectToAction("Index");
+            // return RedirectToAction("FarkliVieweDondur");
+            return RedirectToAction("Index", "Home");
+        }
+        public RedirectToRouteResult RouteYonlendir()
+        {
+            // Bir action içerisinde direkt bir routea yönlendirme yapabiliriz
+            return RedirectToRoute("Default", new { controller = "Home", action = "Index", id = 18});
+        }
+        public PartialViewResult KategorileriGetirPartial()
+        {
+            return PartialView("_PartialMenu");
+        }
+        public PartialViewResult PartialdaModelKullanimi()
+        {
+            var kullanicilar = db.Uyeler.ToList();
+            return PartialView("_PartialModelKullanimi", kullanicilar); // 2. parametre model datası
+        }
+        public IActionResult JsonResult()
+        {
+            var kullanicilar = db.Uyeler.ToList();
+            return Json(kullanicilar);
+        }
+    }
+}
